64f48658ba4d7ba240ce03b6cccb2ed746f0d17e
Gianluca Petrillo <petrillo@slac.stanford.edu>
2025-10-31 02:10:27 -0500

    Added a bit of documentation to TrackingTypeUtils.h

--- lardataalg/Utilities/TrackingTypeUtils.h
+++ lardataalg/Utilities/TrackingTypeUtils.h
@@ -1,3 +1,11 @@
+/**
+ * @file  lardataalg/Utilities/TrackingTypeUtils.h
+ * @brief A few utility functions dealing with `tracking` data types.
+ * @date  October 30, 2025
+ *
+ * The original content of this library was moved from `lardataobj` in LArSoft `v10_12_01`.
+ */
+
 #ifndef LARDATAALG_UTILITIES_TRACKINGTYPEUTILS_H
 #define LARDATAALG_UTILITIES_TRACKINGTYPEUTILS_H
 
@@ -11,8 +19,10 @@
 
 namespace recob::tracking {
 
-  /// @{
   /// Tools to aide the conversion from TVector3 to Point_t and Vector_t
+  /// @{
+
+  /// Copies a vector of points of type `From` into a new vector of points of type `To`.
   template <typename To, typename From>
   std::vector<To> convertVec(std::vector<From> const& in)
   {
@@ -22,26 +32,35 @@ namespace recob::tracking {
       out.push_back(To(i));
     return out;
   }
+
+  /// Returns a copy of the point-like object as a `recob::tracking::Point_t`.
   template <typename Point>
   Point_t toPoint(Point const& p)
   {
     return geo::vect::convertTo<Point_t>(p);
   }
+
+  /// Copies a vector of points of type `Point` into a new vector of `recob::tracking::Point_t`.
   template <typename Point>
   std::vector<Point_t> convertCollToPoint(std::vector<Point> const& coll)
   {
     return geo::vect::convertCollTo<Point_t>(coll);
   }
+
+  /// Returns a copy of the vector-like object as a `recob::tracking::Vector_t`.
   template <typename Vector>
   Vector_t toVector(Vector const& p)
   {
     return geo::vect::convertTo<Vector_t>(p);
   }
+
+  /// Copies a vector of vector-like objects of type `Vector` into a new vector of `recob::tracking::Vector_t`.
   template <typename Vector>
   std::vector<Vector_t> convertCollToVector(std::vector<Vector> const& coll)
   {
     return geo::vect::convertCollTo<Vector_t>(coll);
   }
+
   /// @}
 
 }
